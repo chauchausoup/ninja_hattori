6b5142aa9ababaa513dfb6e937e345a95d0dcb9e
Krishna <7krishna7dhakal7@gmail.com>
2021-04-11 22:59:19 +0545

    TAW for today

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useContext, createContext, useState } from "react";
 //react router dom
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link,Redirect,useHistory,useLocation } from "react-router-dom";
 
 import Login from "../login/Login";
 import Signup from "../signup/Signup";
@@ -8,35 +8,39 @@ import CardList from "../cardList/CardList";
 
 function Homepage() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/login">
-              <button>Login</button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/signup">
-              <button>Signup</button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/user-list">Cardlist</Link>
-          </li>
-        </ul>
-      </nav>
+    <ProvideAuth>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/login">
+                  <button>Login</button>
+                </Link>
+              </li>
+              <li>
+                <Link to="/signup">
+                  <button>Signup</button>
+                </Link>
+              </li>
+              <li>
+                <Link to="/user-list">Cardlist</Link>
+              </li>
+            </ul>
+          </nav>
 
-      <Route path="/login">
-        <Login />
-      </Route>
-      <Route path="/signup">
-        <Signup />
-      </Route>
-      <Route path="/user-list">
-        <CardList />
-      </Route>
-    </div>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/user-list">
+            <CardList />
+          </Route>
+        </div>
+      </Router>
+    </ProvideAuth>
   );
 }
 
