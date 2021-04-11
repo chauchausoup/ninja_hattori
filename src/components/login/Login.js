e549fcbe9c11fff9086903997a259046138a6482
Krishna <7krishna7dhakal7@gmail.com>
2021-04-11 22:34:59 +0545

    will add protected routes later

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,17 +16,80 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const usernameSet = (e) => {
+    e.preventDefault();
+    setUsername(e.target.value);
+  };
+
+  const passwordSet = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+  };
+
+  const handleLoginSuccess = () => {
+    console.log("login success");
+    //clear text box values
+    //got to /user-list page
+    
+
+
+
+
+  };
+
+  const handleLoginFailure = () => {
+    console.log("login failure");
+  };
+
+  const loginHandle = () => {
+    // console.log("login")
+    // console.log(postAPIvalue().password,postAPIvalue().username);
+
+    const getCredentialsInfo = cookies.get("credentialInfo");
+
+    // console.log(getCredentialsInfo)
+
+    getCredentialsInfo &&
+    getCredentialsInfo.username === username &&
+    getCredentialsInfo.password === password
+      ? handleLoginSuccess()
+      : handleLoginFailure();
+  };
+
+  // const postAPIvalue = () => {
+  //   return {
+  //     username: username,
+  //     password: password,
+  //   };
+  // };
+
   const classes = useStyles();
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="outlined-basic" label="username" variant="outlined" />
+      <TextField
+        id="outlined-basic"
+        label="username"
+        variant="outlined"
+        onChange={usernameSet}
+      />
       <br />
 
-      <TextField id="outlined-basic" label="password" variant="outlined" />
+      <TextField
+        id="outlined-basic"
+        label="password"
+        variant="outlined"
+        onChange={passwordSet}
+        type="password"
+      />
       <br />
 
-      <Button variant="contained">Login</Button>
+      <Button variant="contained" onClick={loginHandle}>
+        Login
+      </Button>
     </form>
   );
 }
--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+
+import Cookies from "universal-cookie";
+const cookies = new Cookies()
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,18 +15,63 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Signup() {
+export default function SignUp() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const usernameSet = (e) => {
+    e.preventDefault();
+    setUsername(e.target.value);
+  };
+
+  const passwordSet = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+  };
+
+
+
+  const signupHandle = () => {
+
+    console.log("post api value")
+    console.log(postAPIvalue());
+
+    cookies.set('credentialInfo',postAPIvalue(),{path:'/'})
+  };
+
+
+
+  const postAPIvalue = () => {
+    return {
+      username: username,
+      password: password,
+    };
+  };
+
   const classes = useStyles();
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="outlined-basic" label="username" variant="outlined" />
+      <TextField
+        id="outlined-basic"
+        label="username"
+        variant="outlined"
+        onChange={usernameSet}
+      />
       <br />
 
-      <TextField id="outlined-basic" label="password" variant="outlined" />
+      <TextField
+        id="outlined-basic"
+        label="password"
+        variant="outlined"
+        onChange={passwordSet}
+        type="password"
+      />
       <br />
 
-      <Button variant="contained">SignUp</Button>
+      <Button variant="contained" onClick={signupHandle}>
+        SignUp
+      </Button>
     </form>
   );
 }
